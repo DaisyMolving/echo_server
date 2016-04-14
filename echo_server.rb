f85105854093cae3152d6d37fffcf735c4fdf78e
Daisy Molving <daisymolving@gmail.com>
2016-04-14 14:45:52 +0100

    created method in fake_writer called welcome message and last message printed, one which modifies the output and the other which displays the output. tests pass

--- echo_server.rb
+++ echo_server.rb
@@ -3,5 +3,10 @@ class EchoServer
   def initialize(reader, writer)
 	 @reader = reader
 	 @writer = writer
+	 @welcome_message = "Welcome, please enter something to echo:"
+  end
+
+  def start
+	 @writer.welcome_message(@welcome_message)
   end
 end
--- fake_console_writer.rb
+++ fake_console_writer.rb
@@ -1,3 +1,15 @@
 class FakeWriter
 
+  def initialize
+	 @output = ""
+  end
+
+  def welcome_message(message)
+	 @output = message
+  end
+
+  def last_message_printed
+	 @output
+  end
+
 end
